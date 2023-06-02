99fb2495cd370ceb9d70c53d5ff589617bbf3c58
koya0228 <123162241+koya0228@users.noreply.github.com>
2023-06-02 12:26:22 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -250,6 +250,7 @@
 		
 		let img = document.createElement('img');
 		img.src = dataObject.imgObject.src;
+		body.appendChild(img);
 
 		dataObject.imgWidth = img.width;
 		dataObject.imgHeight = img.height;
